85226f2261e8c76021c223e37fce4511dd70786c
Mike Jackson <perigran_falcon@yahoo.com>
2026-07-05 15:15:14 -0600

    Train keeps a rollin', all night long.

---
+++ Effects_Parallax_Barrier/static/js/main.js
@@ -0,0 +1,84 @@
+// main.js
+
+import * as THREE from "three";
+import { ParallaxBarrierEffect } from "three/addons/effects/ParallaxBarrierEffect.js";
+import { urls, spheres } from "./utils/utilities.js";
+
+let mouseX = 0;
+let mouseY = 0;
+let width = window.innerWidth;
+let height = window.innerHeight;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+
+function main() {
+  // Scene.
+  const scene = new THREE.Scene();
+  const textureCube = new THREE.CubeTextureLoader().load(urls);
+  scene.background = textureCube;
+
+  // Camera.
+  const camera = new THREE.PerspectiveCamera(60, width / height, 0.01, 100);
+  camera.position.z = 3;
+
+  // Renderer.
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setAnimationLoop(animate);
+  const container = document.querySelector("#threejs-container");
+  container.appendChild(renderer.domElement);
+
+  // Spheres.
+  const geometry = new THREE.SphereGeometry(0.1, 32, 16);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    envMap: textureCube,
+  });
+  for (let i = 0; i < 500; i++) {
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = Math.random() * 10 - 5;
+    mesh.position.y = Math.random() * 10 - 5;
+    mesh.position.z = Math.random() * 10 - 5;
+    mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+    scene.add(mesh);
+    spheres.push(mesh);
+  }
+
+  // Parallax Barrier Effect
+  const effect = new ParallaxBarrierEffect(renderer);
+  effect.setSize(width, height);
+
+  // On mouse move.
+  document.addEventListener("mousemove", onDocumentMouseMove);
+  function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowHalfX) / 100;
+    mouseY = (event.clientY - windowHalfY) / 100;
+  }
+
+  // Window resize.
+  window.addEventListener("resize", onWindowResize);
+  function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    effect.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  // Main animation loop.
+  function animate() {
+    const timer = 0.0001 * Date.now();
+    camera.position.x += (mouseX - camera.position.x) * 0.05;
+    camera.position.y += (-mouseY - camera.position.y) * 0.05;
+    camera.lookAt(scene.position);
+
+    for (let i = 0, il = spheres.length; i < il; i++) {
+      const sphere = spheres[i];
+      sphere.position.x = 5 * Math.cos(timer + i);
+      sphere.position.y = 5 * Math.sin(timer + i * 1.1);
+    }
+    effect.render(scene, camera);
+  }
+}
+
+main();
---
+++ Effects_Parallax_Barrier/static/js/utils/utilities.js
@@ -0,0 +1,16 @@
+// utilities.js
+
+// For the sphere geometry.
+export const spheres = [];
+
+// For the texturecube.
+const path = "static/textures/";
+const format = ".png";
+export const urls = [
+  path + "px" + format,
+  path + "nx" + format,
+  path + "py" + format,
+  path + "ny" + format,
+  path + "pz" + format,
+  path + "nz" + format,
+];
