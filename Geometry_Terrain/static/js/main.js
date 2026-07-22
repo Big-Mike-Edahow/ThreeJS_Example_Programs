a6f152360dde8a3bff5dbaf382b035f283ca1a59
Mike Jackson <perigran_falcon@yahoo.com>
2026-07-22 14:38:48 -0600

    Quite pleased with the results.

---
+++ Geometry_Terrain/static/js/main.js
@@ -0,0 +1,162 @@
+// main.js
+
+import * as THREE from "three";
+import Stats from "three/addons/libs/stats.module.js";
+import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
+import { ImprovedNoise } from "three/addons/math/ImprovedNoise.js";
+
+const worldWidth = 256;
+const worldDepth = 256;
+const timer = new THREE.Timer();
+timer.connect(document);
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+function main() {
+  // Scene.
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xefd1b5);
+  scene.fog = new THREE.FogExp2(0xefd1b5, 0.0025);
+
+  // Camera.
+  const camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
+  camera.position.set(100, 800, -800);
+  camera.lookAt(-100, 810, -800);
+
+  // Renderer.
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animate);
+  const container = document.querySelector("#threejs-container");
+  container.appendChild(renderer.domElement);
+
+  // First person controls.
+  const controls = new FirstPersonControls(camera, renderer.domElement);
+  controls.movementSpeed = 150;
+  controls.lookSpeed = 0.1;
+
+  // Stats.
+  const stats = new Stats();
+  container.appendChild(stats.dom);
+
+  // Terrain geometry.
+  const geometry = new THREE.PlaneGeometry(
+    7500,
+    7500,
+    worldWidth - 1,
+    worldDepth - 1,
+  );
+  geometry.rotateX(-Math.PI / 2);
+  const data = generateHeight(worldWidth, worldDepth);
+  const vertices = geometry.attributes.position.array;
+  for (let i = 0, j = 0, l = vertices.length; i < l; i++, j += 3) {
+    vertices[j + 1] = data[i] * 10;
+  }
+
+  // Terrain texture.
+  const texture = new THREE.CanvasTexture(
+    generateTexture(data, worldWidth, worldDepth),
+  );
+  texture.wrapS = THREE.ClampToEdgeWrapping;
+  texture.wrapT = THREE.ClampToEdgeWrapping;
+  texture.colorSpace = THREE.SRGBColorSpace;
+  
+  // Terrain mesh.
+  const terrain = new THREE.Mesh(
+    geometry,
+    new THREE.MeshBasicMaterial({ map: texture }),
+  );
+  scene.add(terrain);
+
+  // Generates a heightmap array for 3D terrain using 3D Perlin noise to create
+  // realistic, layered height variations suitable for height-based meshes.
+  function generateHeight(width, height) {
+    let seed = Math.PI / 4;
+    window.Math.random = function () {
+      const x = Math.sin(seed++) * 10000;
+      return x - Math.floor(x);
+    };
+    const size = width * height,
+      data = new Uint8Array(size);
+    const perlin = new ImprovedNoise(),
+      z = Math.random() * 100;
+    let quality = 1;
+    for (let j = 0; j < 4; j++) {
+      for (let i = 0; i < size; i++) {
+        const x = i % width,
+          y = ~~(i / width);
+        data[i] += Math.abs(
+          perlin.noise(x / quality, y / quality, z) * quality * 1.75,
+        );
+      }
+      quality *= 5;
+    }
+    return data;
+  }
+
+  // Heightmap texture generator. Calculates lighting using the
+  // surface normals of the heightmap and adds noise.
+  function generateTexture(data, width, height) {
+    let context, image, imageData, shade;
+    const vector3 = new THREE.Vector3(0, 0, 0);
+    const sun = new THREE.Vector3(1, 1, 1);
+    sun.normalize();
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    context = canvas.getContext("2d");
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, width, height);
+    image = context.getImageData(0, 0, canvas.width, canvas.height);
+    imageData = image.data;
+    for (let i = 0, j = 0, l = imageData.length; i < l; i += 4, j++) {
+      vector3.x = data[j - 2] - data[j + 2];
+      vector3.y = 2;
+      vector3.z = data[j - width * 2] - data[j + width * 2];
+      vector3.normalize();
+      shade = vector3.dot(sun);
+      imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007);
+      imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007);
+      imageData[i + 2] = shade * 96 * (0.5 + data[j] * 0.007);
+    }
+    context.putImageData(image, 0, 0);
+    const canvasScaled = document.createElement("canvas");
+    canvasScaled.width = width * 4;
+    canvasScaled.height = height * 4;
+    context = canvasScaled.getContext("2d");
+    context.scale(4, 4);
+    context.drawImage(canvas, 0, 0);
+    image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
+    imageData = image.data;
+    for (let i = 0, l = imageData.length; i < l; i += 4) {
+      const v = ~~(Math.random() * 5);
+      imageData[i] += v;
+      imageData[i + 1] += v;
+      imageData[i + 2] += v;
+    }
+    context.putImageData(image, 0, 0);
+    return canvasScaled;
+  }
+
+  // Window resize.
+  window.addEventListener("resize", onWindowResize);
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  // Main animation loop.
+  function animate(timestamp) {
+    timer.update(timestamp);
+    const delta = timer.getDelta();
+
+    controls.update(delta);
+    renderer.render(scene, camera);
+    stats.update();
+  }
+}
+
+main();
